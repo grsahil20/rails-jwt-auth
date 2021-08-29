b383363e004173af0a58b8fe90a0f8874248c886
Sahil Grover <gr.sahil20@gmail.com>
2021-08-29 22:40:55 +0200

    more explicit errors on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    unless params.dig(:user, :email).present? && params.dig(:user, :password).present?
+      return render json: { error: 'User Email and password are required' }, status: :unprocessable_entity
+    else
+      super
+    end
+  end
+
+
   private
   def respond_with(resource, _opts = {})
     render json: resource
